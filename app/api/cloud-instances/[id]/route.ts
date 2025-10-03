a8a1a7e64e15e49087ae04ae1b3dc1c65e99ff1b
mauvpark <mauvpark@gmail.com>
2025-10-03 19:48:27 +0900

    클라우드 조회 api 추가

---
+++ app/api/cloud-instances/[id]/route.ts
@@ -0,0 +1,28 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { NextResponse } from "next/server";
+import { Cloud } from "@/types";
+
+interface Params {
+	id: string;
+}
+
+export async function GET(request: Request, { params }: { params: Params }) {
+	try {
+		const { id } = params;
+		const jsonDirectory = path.join(process.cwd(), "mocks");
+		const fileContents = await fs.readFile(
+			path.join(jsonDirectory, "data.json"),
+			"utf8"
+		);
+		const data = JSON.parse(fileContents).find(
+			(item: Cloud) => item.id === id
+		);
+		return NextResponse.json(data);
+	} catch (error) {
+		return NextResponse.json(
+			{ error: "Failed to load data" },
+			{ status: 500 }
+		);
+	}
+}
