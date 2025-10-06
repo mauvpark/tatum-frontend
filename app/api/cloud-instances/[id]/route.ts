4a3fba4a108b0716325d370a474bf832e6f72f52
mauvpark <mauvpark@gmail.com>
2025-10-06 19:04:30 +0900

    api 리팩토링

--- app/api/cloud-instances/[id]/route.ts
+++ app/api/cloud-instances/[id]/route.ts
@@ -1,25 +1,31 @@
 import { promises as fs } from "fs";
 import path from "path";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { Cloud } from "@/api/services/cloud/types";
 
-interface Params {
-	id: string;
-}
+type Params = Promise<{ id: string }>;
 
-export async function GET(request: Request, { params }: { params: Params }) {
+export async function GET(
+	request: NextRequest,
+	{ params }: { params: Params }
+) {
 	try {
-		const { id } = params;
+		const { id } = await params;
 		const jsonDirectory = path.join(process.cwd(), "mocks");
 		const fileContents = await fs.readFile(
 			path.join(jsonDirectory, "data.json"),
 			"utf8"
 		);
-		const data = JSON.parse(fileContents).find(
-			(item: Cloud) => item.id === id
-		);
+		const allData: Cloud[] = JSON.parse(fileContents);
+		const data = allData.find((item: Cloud) => item.id === id);
+
+		if (!data) {
+			return NextResponse.json({ error: "Not Found" }, { status: 404 });
+		}
+
 		return NextResponse.json(data);
 	} catch (error) {
+		console.error("Failed to load data:", error); // 에러 로깅 추가
 		return NextResponse.json(
 			{ error: "Failed to load data" },
 			{ status: 500 }
